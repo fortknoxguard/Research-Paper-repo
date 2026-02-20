28f806b70688d57a1a95116437d6d42e72729134
fortknoxguard <127070951+fortknoxguard@users.noreply.github.com>
2026-02-21 07:40:39 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import { auth } from "./firebase.js";
 
 const SUPABASE_URL = "https://tgciqknubmwinyykuuve.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // Use your full key
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnY2lxa251Ym13aW55eWt1dXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNTA2NDMsImV4cCI6MjA4NTgyNjY0M30.eO5YV5ip9e4XNX7QtfZAnrMx_vCCv_HQSfdhD5HhKYk"; // Use your full key
 const BUCKET_NAME = "research papers"; 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
