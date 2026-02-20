59d8ab8b44ac5074e5f8b99bcc15606d3ad55e40
fortknoxguard <127070951+fortknoxguard@users.noreply.github.com>
2026-02-20 23:44:41 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const displayName = user.displayName || user.email.split("@")[0];
+    const greetingEl = document.getElementById("user-greeting");
+    if (greetingEl) {
+      greetingEl.textContent = `Welcome, ${displayName}!`;
+    }
+  } else {
+    window.location.href = "index.html";
+  }
+});
