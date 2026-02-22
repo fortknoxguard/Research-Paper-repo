33fd934075e8df207d0a93444d70522c1f07e6c2
fortknoxguard <127070951+fortknoxguard@users.noreply.github.com>
2026-02-22 10:09:40 +0800

    Update research.js

--- research.js
+++ research.js
@@ -4,7 +4,7 @@ const SUPABASE_URL = "https://tgciqknubmwinyykuuve.supabase.co";
 const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnY2lxa251Ym13aW55eWt1dXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNTA2NDMsImV4cCI6MjA4NTgyNjY0M30.eO5YV5ip9e4XNX7QtfZAnrMx_vCCv_HQSfdhD5HhKYk";
 const supabase = createClient(SUPABASE_URL, ANON_KEY);
 
-const BUCKET_ID = "research-papers"; 
+const BUCKET_ID = "research papers"; 
 const STORAGE_BASE_URL = `${SUPABASE_URL}/storage/v1/object/public/${BUCKET_ID}`;
 
 let allPapers = []; 
