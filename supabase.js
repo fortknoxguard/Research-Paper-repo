fb15249918125ee2566650b8b4cc1bf11c480fe0
fortknoxguard <127070951+fortknoxguard@users.noreply.github.com>
2026-02-23 09:22:43 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,10 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
 const SUPABASE_URL = 'https://tgciqknubmwinyykuuve.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnY2lxa251Ym13aW55eWt1dXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNTA2NDMsImV4cCI6MjA4NTgyNjY0M30.eO5YV5ip9e4XNX7QtfZAnrMx_vCCv_HQSfdhD5HhKYk';
 
-const supabase = lib.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 
 window.supabase = supabase;
